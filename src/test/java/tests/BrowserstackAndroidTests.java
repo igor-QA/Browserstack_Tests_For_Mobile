475d0c9906bcd2c5b923e9d01f53bf857b39a29c
Igor Pavlov <igortvk@ya.ru>
2021-02-03 18:15:22 +0300

    added tests for android and iOS platform

--- src/test/java/tests/BrowserstackAndroidTests.java
+++ src/test/java/tests/BrowserstackAndroidTests.java
@@ -7,7 +7,7 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.MobileBy.AccessibilityId;
@@ -19,11 +19,20 @@ public class BrowserstackAndroidTests extends TestBase {
     @Story("Search Test")
     @Tag("android")
     @Test
-    @DisplayName("Successful search in wikipedia android app ")
-    public void abndroidTets() {
+    @DisplayName("Successful search in wikipedia android platform ")
+    public void successfulSearchInWiki() {
             open();
             $(AccessibilityId("Search Wikipedia")).click();
             $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
             $$(className("android.widget.TextView")).shouldBe(CollectionCondition.sizeGreaterThan(0));
+    }
+
+    @Test
+    @DisplayName("GoogleMaps")
+    public void checkSearchCityViaGMaps() {
+        open();
+        $(AccessibilityId("Google Maps")).click();
+        $(MobileBy.className("android.widget.TextView")).setValue("Muenchen");
+        $$(AccessibilityId("com.google.android.apps.maps:id/map_frame")).shouldBe(CollectionCondition.sizeGreaterThan(0));
     }
 }
--- src/test/java/tests/BrowserstackiOSTests.java
+++ src/test/java/tests/BrowserstackiOSTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.appium.java_client.MobileBy;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
@@ -18,11 +19,20 @@ public class BrowserstackiOSTests extends TestBase {
     @Tag("ios")
     @Test
     @DisplayName("Successful search in ios platform")
-    public void iOSTets() {
+    public void successfulSearchText() {
 
         open();
         $(AccessibilityId("Text Button")).click();
         $(AccessibilityId("Text Input")).setValue("hello@browserstack.com").pressEnter();
         $(AccessibilityId("Text Output")).shouldHave(text("hello@browserstack.com"));
+    }
+
+    @Test
+    @DisplayName("Successful search city in Google maps")
+    public void checkSearchCityViaGMaps() {
+        open();
+        $(MobileBy.className("Google Maps")).click();
+        $(MobileBy.className("SearchHeaderSearchTxtView")).setValue("Muenchen");
+        $(MobileBy.className("Directions")).shouldHave(text("Directions"));
     }
 }
